0e907f024390a2dca6c90da7a488cf8e688f0bf2
ablades <audaris@gmail.com>
2019-11-03 17:13:48 -0800

    create flask index route

--- Code/app.py
+++ Code/app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from histogram import read_file, histogram_dictonary
+from sample import better_words
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World"
+    words = read_file('hist_test.txt')
+    histogram = histogram_dictonary(words)
+    sentence = better_words(10, len(words), histogram)
+    return sentence
