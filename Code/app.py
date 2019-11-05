03a38180562d8992e5e541ee64a834d400e9d770
ablades <audaris@gmail.com>
2019-11-04 18:58:50 -0800

    lay front end groundwork

--- Code/app.py
+++ Code/app.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 
 from histogram import read_file, histogram_dictonary
 from sample import better_words
 
 
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/', methods=['GET', 'POST'])
+def index():
     words = read_file('hist_test.txt')
+    number_of_words = 10
     histogram = histogram_dictonary(words)
-    sentence = better_words(10, len(words), histogram)
+
+    #user has inputed a number for the sentence length
+    if request.method == 'POST':
+        number_of_words = request.form.get('word_count')
+
+    sentence = better_words(number_of_words, len(words), histogram)
     return render_template('index.html', sentence=sentence)
