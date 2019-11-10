583a56a81530eceb6777ed4b65f448a6b0e067ab
ablades <audaris@gmail.com>
2019-11-09 16:42:38 -0800

    add markov sentence gen

--- Code/markov.py
+++ Code/markov.py
@@ -1,32 +1,49 @@
 from dictogram import Dictogram
 from utils import cleanup_source
+import random
 
 
-
-def create_markov_chain(words_list):
+def create_state_histogram(words_list):
     """
         Create markov chain from given words list
     """
-    markov_dictonary = dict()
+    state_histogram = dict()
 
     #loop through words list
     for index, word in enumerate(words_list):
 
-        #Check if word is in dictonary already
-        if markov_dictonary.get(word) == None:
+        #Check if word is in histogram already
+        if state_histogram.get(word) == None:
             #set value to a new dictogram object
-            markov_dictonary[word] = Dictogram()
+            state_histogram[word] = Dictogram()
 
         #Make sure list does not go out of range
         if index + 1 < len(words_list) - 1:
             next_word = words_list[index + 1]
             #add next word to chain
-            markov_dictonary.get(word).add_count(next_word)
+            state_histogram.get(word).add_count(next_word)
 
-    return markov_dictonary
+    return state_histogram
+
+
+def create_sentence(state_histogram, length):
+    #randomly choose a word to start sentence 
+    sampled_word = random.choice(list(state_histogram))
+
+    sentence = sampled_word
+
+    for item in range(length - 1):
+
+        sampled_word = state_histogram[sampled_word].sample()
+        sentence += " " + sampled_word
+
+    return sentence
+
 
 if __name__ == "__main__":
     
     words_list = cleanup_source('hist_test.txt')
 
-    print(create_markov_chain(words_list))
+    state_histogram = create_state_histogram(words_list)
+
+    print(create_sentence(state_histogram, 10))
